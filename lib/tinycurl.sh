d43675dd5378d4ce2a7fa7d6c3e0c1589e05bc06
Xi__Han <96098265+XiHanQWQ@users.noreply.github.com>
2025-07-15 01:42:25 +0800

    Update tinycurl.sh

--- lib/tinycurl.sh
+++ lib/tinycurl.sh
@@ -3,6 +3,7 @@ export CC="i686-w64-mingw32-gcc"
 export CFLAGS="-Os -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables"
 export LDFLAGS="-Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections -static-libgcc"
 mkdir -p build32 && cd build32 && ../configure --host i686-w64-mingw32 \
+--enable-proxy \
 --disable-pthreads \
 --enable-static \
 --disable-shared \
